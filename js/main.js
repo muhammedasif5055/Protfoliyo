693ef27d9d598695619c882d178913cf26a57f41
Asif <muhammedasif5055@gmail.com>
2021-10-29 22:28:16 +0530

    comment section added

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ about section
  })
 })();
 function bodyScrollingToggle(){
-   document.body.classList.toggle("stop-scrolling");
+   document.body.classList.toggle("hidden-scrolling");
 }
 /*------------------------portfolio filter and popup-------------------------*/
 
@@ -81,6 +81,9 @@ function bodyScrollingToggle(){
    })
    closeBtn.addEventListener("click",()=>{
       popupToggle();
+      if(projectDetailsContainer.classList.contains("active")){
+         popupDetailsToggle();
+      }
    })
    function popupToggle () {
       popup.classList.toggle("open")
@@ -107,7 +110,7 @@ function bodyScrollingToggle(){
          slideIndex++;
       }
       popupSlideshow();
-      console.log("slideInsex:" + slideIndex);
+    
    })
    // prev sllide
    prevBtn.addEventListener("click",() =>{
@@ -118,7 +121,6 @@ function bodyScrollingToggle(){
          slideIndex--;
       }
       popupSlideshow();
-      console.log("slideInsex:" + slideIndex);
    })
 
    function popupDetails(){
@@ -156,4 +158,50 @@ function bodyScrollingToggle(){
       popup.scrollTo(0,projectDetailsContainer.offsetTop)
    }
    }
+})();
+
+/*---------------------- testimonial slider ---------------------------- */
+(()=>{
+const sliderContainer = document.querySelector(".testi-slider-container"),
+slides = sliderContainer.querySelectorAll(".testi-item"),
+slideWidth = sliderContainer.offsetWidth,
+prevBtn = document.querySelector(".testi-slider-nav .prev"),
+nextBtn = document.querySelector(".testi-slider-nav .next");
+activeSlide = sliderContainer.querySelector(".testi-item.active")
+let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(activeSlide);
+
+//set width of all slides
+slides.forEach ((slide)=>{
+   slide.style.width = slideWidth + "px";
+})
+// set width of sliderContainer
+sliderContainer.style.width = slideWidth * slides.length + "px";
+
+nextBtn.addEventListener("click",()=>{
+if(slideIndex === slides.length-1){
+   slideIndex = 0;
+}
+else{
+   slideIndex++;
+
+}
+slider()
+})
+prevBtn.addEventListener("click",()=>{
+   if(slideIndex === 0){
+      slideIndex = slides.length-1;
+   }
+   else{
+      slideIndex--;
+   }
+   slider()
+})
+function slider(){
+   //deactive existing active slides
+   sliderContainer.querySelector(".testi-item.active").classList.remove("active");
+   //activate new slide
+   slides[slideIndex].classList.add("active");
+   sliderContainer.style.marginLeft = - (slideWidth * slideIndex) + "px";
+}
+slider();
 })();
